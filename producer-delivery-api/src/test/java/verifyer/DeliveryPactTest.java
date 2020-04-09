5e65519c63e789479803cc6f137952dbcdd51a35
ogulcan.arabaci <123Ogul123>
2020-04-09 14:21:07 +0300

    delete unused lib

--- producer-delivery-api/src/test/java/verifyer/DeliveryPactTest.java
+++ producer-delivery-api/src/test/java/verifyer/DeliveryPactTest.java
@@ -8,36 +8,8 @@ import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import au.com.dius.pact.provider.spring.SpringRestPactRunner;
-import com.oglcnarbc.DeliveryProducerApiApplication;
-import com.oglcnarbc.model.response.CreateDeliveryResponse;
-import com.oglcnarbc.service.DeliveryService;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
 
-import java.net.URL;
-import java.util.Arrays;
-
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRestPactRunner.class)
 @Provider("delivery_provider")
