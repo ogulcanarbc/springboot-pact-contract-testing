99415ffec83e1d51901cb04ebcc9f1f8e415a558
ogulcan.arabaci <123Ogul123>
2020-06-12 12:08:18 +0300

    added pact broker auth cınfig and provider pact jvm test dependency

--- producer-delivery-api/src/test/java/verifyer/DeliveryVerifyPactTest.java
+++ producer-delivery-api/src/test/java/verifyer/DeliveryVerifyPactTest.java
@@ -1,5 +1,6 @@
 package verifyer;
 
+import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
@@ -7,19 +8,21 @@ import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
+
 import au.com.dius.pact.provider.spring.SpringRestPactRunner;
-import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
 import org.junit.runner.RunWith;
 
 
 @RunWith(SpringRestPactRunner.class)
+//@RunWith(PactRunner.class)
 @Provider("delivery_provider")
 @PactBroker(host = "localhost", port = "80",
-        authentication = @PactBrokerAuth(username = "${pactBroker.auth.user}", password = "${pactBroker.auth.password}"))
+        authentication =  @PactBrokerAuth(username = "pact", password = "1234"))
 public class DeliveryVerifyPactTest {
 
+
     @TestTarget
-    public final Target target = new SpringBootHttpTarget();
+    public final Target target = new HttpTarget("http", "localhost", 8099);
 
 
     @State(value = "get one delivery by id")
